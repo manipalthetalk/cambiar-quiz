fecc58456599070a224e3628f91ffe32922ccea6
dhzdhd <ragas5432100@gmail.com>
2022-10-07 09:24:21 +0530

    Add base api and nodemon

--- api/index.js
+++ api/index.js
@@ -1,7 +1,33 @@
-export default function handler(request, response) {
-  response.status(200).json({
-    body: request.body,
-    query: request.query,
-    cookies: request.cookies,
-  });
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import fs from 'fs';
+
+const app = express();
+const port = process.env.PORT ?? 5000;
+
+const serverUrl = '';
+const allowedOrigins = ['http://localhost:5000', serverUrl];
+const options = {
+	origin: allowedOrigins
+};
+
+const initData = async () => {
+	fs.readFile('./_files/questions.json','utf8' ,(err, data) => {
+		console.log(data);
+	});
+
 }
+
+app.use(cors(options));
+app.use(bodyParser.json());
+
+app.route('/questions')
+.get(async (req, res) => {
+
+})
+
+app.listen(port, () => {
+	initData()
+	console.log('Listening on http://localhost:5000/')
+});
