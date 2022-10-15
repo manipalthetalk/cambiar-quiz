43c3b234058e2aec7b7c4d851e9802492ac956a4
dhzdhd <ragas5432100@gmail.com>
2022-10-15 20:17:01 +0530

    Add caching of questions

--- api/index.js
+++ api/index.js
@@ -1,33 +1,39 @@
-import express from 'express';
-import cors from 'cors';
-import bodyParser from 'body-parser';
-import fs from 'fs';
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import fs from "fs";
+import mcache from "memory-cache";
 
 const app = express();
 const port = process.env.PORT ?? 5000;
 
-const serverUrl = '';
-const allowedOrigins = ['http://localhost:5000', serverUrl];
+const serverUrl = "https://cambiar-quiz.vercel.app";
+const allowedOrigins = ["http://localhost:5000", serverUrl];
 const options = {
-	origin: allowedOrigins
+  origin: allowedOrigins,
 };
 
-const initData = async () => {
-	fs.readFile('./_files/questions.json','utf8' ,(err, data) => {
-		console.log(data);
-	});
-
-}
+const initData = () => {
+  fs.readFile("./_files/questions.json", "utf8", (err, data) => {
+    mcache.put("questions", data);
+  });
+};
 
 app.use(cors(options));
 app.use(bodyParser.json());
 
-app.route('/questions')
-.get(async (req, res) => {
+app.route("/api/questions").get(async (req, res) => {
+  let data = mcache.get("questions");
 
-})
+  if (data === undefined) {
+    initData();
+    data = mcache.get("questions");
+  }
 
+  res.status(200).send(data);
+});
+
+initData();
 app.listen(port, () => {
-	initData()
-	console.log('Listening on http://localhost:5000/')
+  console.log("Listening on http://localhost:5000/");
 });
