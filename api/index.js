bb360d888df5a1c177fe62b52475a81259785e90
dhzdhd <72333092+dhzdhd@users.noreply.github.com>
2022-10-18 00:40:22 +0530

    Setup serverless function (#4)
    * Add base api and nodemon
    
    * Update schema
    
    * Add caching of questions

--- api/index.js
+++ api/index.js
@@ -1,7 +1,39 @@
-export default function handler(request, response) {
-  response.status(200).json({
-    body: request.body,
-    query: request.query,
-    cookies: request.cookies,
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import fs from "fs";
+import mcache from "memory-cache";
+
+const app = express();
+const port = process.env.PORT ?? 5000;
+
+const serverUrl = "https://cambiar-quiz.vercel.app";
+const allowedOrigins = ["http://localhost:5000", serverUrl];
+const options = {
+  origin: allowedOrigins,
+};
+
+const initData = () => {
+  fs.readFile("./_files/questions.json", "utf8", (err, data) => {
+    mcache.put("questions", data);
   });
-}
+};
+
+app.use(cors(options));
+app.use(bodyParser.json());
+
+app.route("/api/questions").get(async (req, res) => {
+  let data = mcache.get("questions");
+
+  if (data === undefined) {
+    initData();
+    data = mcache.get("questions");
+  }
+
+  res.status(200).send(data);
+});
+
+initData();
+app.listen(port, () => {
+  console.log("Listening on http://localhost:5000/");
+});
