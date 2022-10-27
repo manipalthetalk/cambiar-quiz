929a6a80f759aeed0977c903e369f8a15a3fd92e
KKRISHNA SAXENA <saxena.kkrishna@gmail.com>
2022-10-27 17:34:20 +0530

    Create MenuItems.js

---
+++ src/Components/Navbar/MenuItems.js
@@ -0,0 +1,22 @@
+export const MenuItems=[
+    {
+        title: 'About Us',
+        url:'#',
+        cname:'nav-links'
+    },
+    {
+        title: 'Departments',
+        url:'#',
+        cname:'nav-links'
+    },
+    {
+        title: "Fresher's Guide",
+        url:'#',
+        cname:'nav-links'
+    },
+    {
+        title: 'Our Socials',
+        url:'#',
+        cname:'nav-links'
+    }         
+]
