c587d38cf39f96b299d32a3af6d6cccc12b69183
KKRISHNA SAXENA <saxena.kkrishna@gmail.com>
2022-11-01 10:39:41 +0530

    Add files via upload

---
+++ src/Components/Navbar/MenuItems.js
@@ -0,0 +1,17 @@
+export const MenuItems=[
+    {
+        title: 'About Us',
+        url:'https://freshers.manipalthetalk.org/about',
+        cname:'nav-links'
+    },
+    {
+        title: 'Departments',
+        url:'https://freshers.manipalthetalk.org/departments',
+        cname:'nav-links'
+    },
+    {
+        title: "Fresher's Guide",
+        url:'https://freshers.manipalthetalk.org/',
+        cname:'nav-links'    
+    }   
+]
